e80741282f830f3a645056395a1c97d0ac74df8f
hkhatri <Sandy@Sandeeps-MacBook-Air.local>
2019-01-13 20:08:09 -0600

    Change in settings.py

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k9x1mot@(^vb9!*%020&eyr+v*o77mrt-5kl@3(ukii8atx_jc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
